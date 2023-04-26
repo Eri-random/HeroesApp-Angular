a62589b92dba04df1e91d5fb84a6a505972a61ac
Eri-random <brenherrera@alumno.unlam.edu.ar>
2023-04-26 16:12:32 -0300

    Angular Material

--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroe-page',
@@ -6,6 +10,31 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class HeroePageComponent {
+export class HeroePageComponent implements OnInit{
 
+  public hero?: Hero;
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRouter: ActivatedRoute,
+    private router: Router,
+    ){}
+
+  ngOnInit(): void {
+
+    this.activatedRouter.params
+      .pipe(
+        //delay(30000),
+        switchMap(({id}) => this.heroesService.getHeroById(id)),
+      ).subscribe(hero =>{
+
+        if(!hero) return this.router.navigate(['/heroes/list']);
+
+        this.hero = hero;
+        return;
+      })
+
+  }
+
+  
 }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,3 @@
+export const environments={
+    baseUrl:'https://erika-herrera.com/api'
+}
